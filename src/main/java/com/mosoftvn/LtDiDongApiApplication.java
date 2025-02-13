860d4e17bd6f7730d0d8d71a66cd7838ed3a7bfe
vovantri123 <vovantri204@gmail.com>
2025-02-13 13:48:26 +0700

    Upload project to begin code

---
+++ src/main/java/com/mosoftvn/LtDiDongApiApplication.java
@@ -0,0 +1,13 @@
+package com.mosoftvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtDiDongApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtDiDongApiApplication.class, args);
+	}
+
+}
